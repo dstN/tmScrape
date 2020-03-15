0484efbed6a15bede664201eceb3c2994b623158
dstN <dstN@yinside.de>
2020-03-15 17:25:07 +0100

    more trying.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-var startDate = 1957; // first year a transfer fee was recorded on transfermarkt.com
+var startDate = 1956; // first year a transfer fee was recorded on transfermarkt.com
 var endDate = new Date().getFullYear(); // current Year
-var getUrl = "https://www.transfermarkt.com/transfers/transferrekorde/statistik/top/saison_id/"; // ADD Year after saidson_id/
+var getUrl = "https://www.transfermarkt.com/transfers/transferrekorde/statistik/top/saison_id/"; // ADD Year after saison_id/
 
-for (i=startDate; i <= endDate; i++) { // iterate until current year
-  var tmData = $.get(getUrl + i); // get the page of current year
-  console.log(tmData); 
+for (i=startDate; i <= 1960; i++) { // iterate until current year - change to endDate in production mode
+  $.get(getUrl + i, function(data){ // get the page of current year
+    $(".loader").html(data);
+    var tableRows = $(".loader").find(".items>tbody>tr");
+    tableRows.each(function(){
+      var innerHTML = $(this).html();
+      $(".table>table>tbody").append("<tr>"+innerHTML+"</tr>");
+    });
+  });
 }
