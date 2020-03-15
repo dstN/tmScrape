5e1af17af4ff569b1263d6d43d304016144d32f7
dstN <dstN@yinside.de>
2020-03-15 18:37:47 +0100

    complete obj to json

--- main.js
+++ main.js
@@ -8,7 +8,57 @@ for (i=startDate; i <= 1960; i++) { // iterate until current year - change to en
     var tableRows = $(".loader").find(".items>tbody>tr");
     tableRows.each(function(){
       var innerHTML = $(this).html();
-      $(".table>table>tbody").append("<tr>"+innerHTML+"</tr>");
+      var rank = $(this).find(">td:first-of-type").text();
+      var picture = $(this).find(">td:nth-of-type(2)").find("table>tbody>tr:first-of-type>td:first-of-type>img").attr("src");
+      var playerLink = $(this).find(">td:nth-of-type(2)").find("table>tbody>tr:first-of-type>td.hauptlink>a").attr("href");
+      var playerID = $(this).find(">td:nth-of-type(2)").find("table>tbody>tr:first-of-type>td.hauptlink>a").attr("id");
+      var playerName = $(this).find(">td:nth-of-type(2)").find("table>tbody>tr:first-of-type>td.hauptlink>a").text();
+      var position = $(this).find(">td:nth-of-type(2)").find("table>tbody>tr:last-of-type>td").text();
+      var year = $(this).find(">td:nth-of-type(3)>a").text();
+      var nationalities = $(this).find(">td:nth-of-type(4)>img");
+      if(nationalities.length===2){
+        var nationalityOne = nationalities.eq(0).attr("alt");
+        var nationalityTwo = nationalities.eq(1).attr("alt");
+        var flagOne = nationalities.eq(0).attr("src");
+        var flagTwo = nationalities.eq(1).attr("src");
+      }
+      else {
+        var nationalityOne = nationalities.attr("alt");
+        var flagOne = nationalities.attr("src");
+        var nationalityTwo = "none";
+        var flagTwo = "none";
+      }
+      var joinedClub = $(this).find(">td:nth-of-type(5)").find("table>tbody>tr:first-of-type>td:last-of-type>a").text();
+      var joinedClubLink = $(this).find(">td:nth-of-type(5)").find("table>tbody>tr:first-of-type>td:last-of-type>a").attr("href");
+      var clubEmblem = $(this).find(">td:nth-of-type(5)").find("table>tbody>tr:first-of-type>td:first-of-type>a>img").attr("src");
+      var leagueNationality = $(this).find(">td:nth-of-type(5)").find("table>tbody>tr:last-of-type>td:first-of-type>img").attr("alt");
+      var leagueNationalityFlag = $(this).find(">td:nth-of-type(5)").find("table>tbody>tr:last-of-type>td:first-of-type>img").attr("src");
+      var transferFee = $(this).find(">td:last-of-type>a").text();
+
+
+
+      //$(".table>table>tbody").append("<tr>"+innerHTML+"</tr>");
+
+      var playerObj = {
+        rank: rank,
+        picture: picture,
+        playerLink: "https://www.transfermarkt.com" + playerLink,
+        playerName: playerName,
+        position: position,
+        year: year,
+        nationalityOne: nationalityOne,
+        nationalityTwo: nationalityTwo,
+        flagOne: flagOne,
+        flagTwo: flagTwo,
+        joinedClub: joinedClub,
+        joinedClubLink: "https://www.transfermarkt.com" + joinedClubLink,
+        clubEmblem: clubEmblem,
+        leagueNationality: leagueNationality,
+        leagueNationalityFlag: leagueNationalityFlag,
+        transferFee: transferFee
+      };
+      var playerJSON = JSON.stringify(playerObj);
+      $(".json").append("<p id='"+playerID+"'>"+playerJSON+"</p>");
     });
   });
 }
