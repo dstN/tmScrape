13cc79d8dbd3383e5e807987ee5652043c7cb8ef
dstN <dstN@yinside.de>
2020-03-15 15:35:14 +0100

    first tries

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var startDate = 1957;
-var endDate = new Date().getFullYear();
+var startDate = 1957; // first year a transfer fee was recorded on transfermarkt.com
+var endDate = new Date().getFullYear(); // current Year
+var getUrl = "https://www.transfermarkt.com/transfers/transferrekorde/statistik/top/saison_id/"; // ADD Year after saidson_id/
 
-
-var tmData = $.get("https://www.transfermarkt.de/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=1957");
-
-console.log(tmData);
+for (i=startDate; i <= endDate; i++) { // iterate until current year
+  var tmData = $.get(getUrl + i); // get the page of current year
+  console.log(tmData); 
+}
